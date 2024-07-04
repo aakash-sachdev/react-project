62ed1a653f49757e815b6c5fbd2c37299ba9b478
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-07-04 00:14:54 -0500

    Counter created | Hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+ 
+
+  const addValue = () => {
+    if (counter > 19){
+      counter = 19;
+    }
+    setCounter(counter + 1);
+  }
+  
+  const subValue = () => {
+    if (counter < 1){
+      counter = 1;
+    }
+    setCounter(counter - 1);
+  }
+
+
+  return (
+    <>
+      <h1>2nd Project - Counter</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}> Increase value </button>
+      <br/>
+      <button onClick={subValue}> Decrease value </button>
+    </>
+  )
+}
+
+export default App
