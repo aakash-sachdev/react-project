1ab7b472c3171f9b916fe765a7efc3568feab3e8
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-07-03 22:16:58 -0500

    Add component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,12 @@
-
+import Name from "./Name"
 
 function App() {
 
   return (
+    <>
    <h1>React with Vite project | AS</h1>
+   <Name/>
+   </>
   )
 }
 
---
+++ 01vitereact/src/Name.jsx
@@ -0,0 +1,13 @@
+function Name() {
+    return(
+        <>
+        <ui>
+        <li>Aakash</li>
+        <li>Hitesh</li>
+        <li>Emily</li>
+        </ui>
+        </>
+    )
+}
+
+export default Name
